a46b83171da8b5123c5469c917c6bdb42e1a6ad9
yaschrist <88407056+yaschrist@users.noreply.github.com>
2022-04-24 14:56:15 -0300

    Add files via upload

---
+++ numerointeiro1.cpp
@@ -0,0 +1,15 @@
+//1 - numero inteiro
+
+#include <iostream>
+
+main() {
+	
+	setlocale(LC_ALL, "Portuguese");
+	
+	int numero;
+	
+	printf("Digite um número inteiro: ");
+	scanf("%d",&numero);
+	
+	printf("O número é: %d", numero);
+}
